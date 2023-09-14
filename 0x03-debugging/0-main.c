2e9393492a67f1f6e999a14f57713c37ba4f76af
DennisMakaka <dennismakaka078@gmail.com>
2023-09-14 13:32:38 -0700

    This program converts a date to the day of year and determines how many days are left in the year, taking leap year into consideration.

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,20 +1,12 @@
 #include "main.h"
-
 /**
- * main - Entry point of the program.
- *
- * Description:
- * This function serves as the entry point of the program. It initializes
- * a variable 'i' to 0 and calls the 'positive_or_negative' function.
- *
- * Return: Always returns 0 to indicate successful execution.
- */
+* main - Test function for positive or negative
+* Return: 0
+*/
 int main(void)
 {
-	int i;
-
-	i = 0;
-	positive_or_negative(i);
-
-	return (0);
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
 }
