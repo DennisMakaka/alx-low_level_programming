6d8eb28db8a0784b0c8321f9727fe1bc5494787c
DennisMakaka <dennismakaka078@gmail.com>
2023-09-14 13:14:20 -0700

    Like, comment, subscribe

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * Description: This function causes an infinite loop and then avoids it.
+ *
+ * Return: Always returns 0 to indicate successful execution.
+ */
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+	i = 0;
+
+	/*
+	 * while (i < 10)
+	 * {
+	 *     putchar(i);
+	 * }
+	 */
+
+	printf("Infinite loop avoided! \\o/\n");
+	return (0);
+}
