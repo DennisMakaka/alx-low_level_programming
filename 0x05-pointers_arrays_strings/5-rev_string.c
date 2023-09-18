a30ee31552ad0f14b5a2a086373153ca96537a9b
DennisMakaka <dennismakaka078@gmail.com>
2023-09-18 21:40:40 +0300

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,27 +10,9 @@
  */
 void rev_string(char *s)
 {
-<<<<<<< HEAD
-    int length = 0;
-    char temp;
-    int i; /* Declare 'i' here */
-
-    /* Calculate the length of the string */
-    while (s[length] != '\0')
-    {
-        length++;
-    }
-
-    /* Reverse the string in place */
-    for (i = 0; i < length / 2; i++) /* Move the declaration of 'i' here */
-    {
-        temp = s[i];
-        s[i] = s[length - 1 - i];
-        s[length - 1 - i] = temp;
-    }
-=======
 	int length = 0;
 	char temp;
+	int i; /* Declare 'i' here */
 
 	/* Calculate the length of the string */
 	while (s[length] != '\0')
@@ -39,11 +21,10 @@ void rev_string(char *s)
 	}
 
 	/* Reverse the string in place */
-	for (int i = 0; i < length / 2; i++)
+	for (i = 0; i < length / 2; i++) /* Move the declaration of 'i' here */
 	{
 		temp = s[i];
 		s[i] = s[length - 1 - i];
 		s[length - 1 - i] = temp;
 	}
->>>>>>> 3c6f70437c42b0202cbfba0ffe0e74f0164416c6
 }
