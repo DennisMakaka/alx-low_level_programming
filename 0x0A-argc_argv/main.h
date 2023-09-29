90042f5ea614f20c29103cd7483eaa263d75cdc3
DennisMakaka <dennismakaka078@gmail.com>
2023-09-29 09:04:14 -0700

    This is a a program that prints its name, followed by a new line.

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - Writes a character to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1, on error -1.
+ */
+int _putchar(char c);
+
+/**
+ * _atoi - Converts a string to an integer
+ * @s: The string to convert
+ *
+ * Return: The converted integer
+ */
+int _atoi(char *s);
+
+#endif /* MAIN_H */
+
