964c95dc8977858217983db9218acda3fcebc330
DennisMakaka <dennismakaka078@gmail.com>
2023-10-09 02:52:32 -0700

    This is a script with a header file that defines a macro named PI as an abbreviation for the token 3.14159265359.

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,14 @@
+#ifndef PI_H
+#define PI_H
+
+/*
+ * File: pi.h
+ * Description: Header file for the mathematical constant PI.
+ * Author: Dennis Makaka
+ * Date: 9/10/23
+ */
+
+#define PI 3.14159265359
+
+#endif /* PI_H */
+
