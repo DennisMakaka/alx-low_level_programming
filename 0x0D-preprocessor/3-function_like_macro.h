8aad1c41fde1172ec721213aa2404a170d41bbe9
DennisMakaka <dennismakaka078@gmail.com>
2023-10-09 03:07:19 -0700

    This is a function-like macro ABS(x) that computes the absolute value of a number x

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/*
+ * File: function_like_macro.h
+ * Description: Header file for macro to compute the absolut value of a number.
+ * Author: Dennis Makaka
+ * Date: 9/10/23
+ */
+
+#define ABS(x) ((x) < (0) ? -(x) : (x))
+
+#endif /* FUNCTION_LIKE_MACRO_H */
