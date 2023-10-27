c653a76e77239cd9a172a4585dfad59fa1c022d5
DennisMakaka <dennismakaka078@gmail.com>
2023-10-27 00:20:52 -0700

    This is a function thet returns the value of a bit ata given index.

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * _pow - Calculates the result of base raised to the power
+ * _pow - Calculates the result of base raised to the power.
  * @base: Base of the exponent.
- * @power: Power exponent.
+ * @power: Power of the exponent.
  *
- * Return: The value of (base ^ power)
+ * Return: The value of (base ^ power).
  */
 unsigned long int _pow(unsigned int base, unsigned int power)
 {
-	unsigned long int num;
-	unsigned int a;
+unsigned long int num;
+unsigned int a;
 
-	num = 1;
-	for (a = 1; a <= power; a++)
-		num *= base;
-	return (num);
+num = 1;
+for (a = 1; a <= power; a++)
+num *= base;
+return (num);
 }
 
 /**
@@ -26,23 +26,23 @@ unsigned long int _pow(unsigned int base, unsigned int power)
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int divisor, check;
-	char flag;
+unsigned long int divisor, check;
+char flag;
 
-	flag = 0;
-	divisor = _pow(2, sizeof(unsignd long int) * 8 - 1);
-	while (divisor != 0)
-	{
-		check = n & divisor;
-		if (check == divisor)
-		{
-			flag = 1;
-			_putchar('1');
-		}
-		else if (flag == 1 || divisor == 1)
-		{
-			_putchar('0');
-		}
-		divisor >>= 1;
-	}
+flag = 0;
+divisor = _pow(2, sizeof(unsigned long int) * 8 - 1);
+while (divisor != 0)
+{
+check = n & divisor;
+if (check == divisor)
+{
+flag = 1;
+_putchar('1');
+}
+else if (flag == 1 || divisor == 1)
+{
+_putchar('0');
+}
+divisor >>= 1;
+}
 }
